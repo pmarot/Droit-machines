be05a39b60d3635125a6be5555c57c83cfd5bd3d
pmarot <apmarot@gmail.com>
2018-06-29 09:01:08 +0400

    ajout ejs et finalisation architecture fichiers

---
+++ index.js
@@ -0,0 +1,18 @@
+// server.js
+// load the things we need
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page 
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+
+app.listen(3005);
+console.log('Tout est ok');
